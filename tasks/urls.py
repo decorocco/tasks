117814355a80e259252609b57e2fc52a48b23080
André Rocco <49247925+decorocco@users.noreply.github.com>
2021-12-03 19:38:06 -0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from views import index, listTask,postTask,deleteTask
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('get', listTask, name='getTasks'),
-    path('post',postTask, name='postTask'),
-    path('delete/<int:id>',deleteTask,name='deleteTask')
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
