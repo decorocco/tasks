f0d444ae144f54cb947906b8bd8207336ebed159
André Rocco <49247925+decorocco@users.noreply.github.com>
2021-12-03 19:27:45 -0300

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from tasks.views import TasksViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TasksViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
