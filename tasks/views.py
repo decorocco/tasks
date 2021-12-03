124a39e4a4faa3f38c3a30ee9a39680cbda3c758
André Rocco <49247925+decorocco@users.noreply.github.com>
2021-12-03 19:36:11 -0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,6 @@ from rest_framework import viewsets
 from .models import Task
 from .serializer import TaskSerializer
 
-# def index(request):
-#     all_tasks = Task.objects.all()
-#     return HttpResponse(all_tasks)
-
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
