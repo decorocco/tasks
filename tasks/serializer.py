b73e677505d17b3fff544fef4f37a3fb3b0d980f
André Rocco <49247925+decorocco@users.noreply.github.com>
2021-12-03 19:31:42 -0300

    Update serializer.py

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from tasks.models import Task
+from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ("id", "title", "pub_date", "description")
+        fields = ["title", "pub_date", "description"]
